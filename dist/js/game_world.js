770af40af8f9c91bbdfaeaa7d96c2b9c4897e1f2
Tor Vegard <katten442@gmail.com>
2018-09-29 20:42:37 +0200

    Initial commit

---
+++ dist/js/game_world.js
@@ -0,0 +1,26 @@
+const default_types = [
+    'default',
+    ''
+]
+const default_game_world_size = {
+    x: 7,
+    y: 3,
+    z: 9
+};
+
+function world (type) {
+    
+    function init_world(type, size){
+        if (type == default || null) {
+            for 
+        }
+    }
+
+    function building_slot(type) {
+        this.type = type;
+
+    }
+}
+  
+
+game_world = world()
